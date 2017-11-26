84e3f8cba0affcc872b811588d648dc18e8f86f0
cmderQ <1257698043@qq.com>
2017-11-26 22:21:51 +0800

    入口函数

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 入口文件
+ * 1.定义常量
+ * 2.加载函数库
+ * 3.启动框架
+ */
+define('FRAMEWORK', realpath('./'));
+define('CORE', FRAMEWORK . './core');
+define('APP', FRAMEWORK . './APP');
+
+define('DEBUG', true);
+
+if (DEBUG) {
+    ini_set('display_errors', 'On');
+} else {
+    ini_set('display_errors', 'Off');
+}
+
+include CORE.'/common/function.php';
+
+op(FRAMEWORK);
