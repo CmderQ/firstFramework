a2577702fc87c8fa0b254adefe59b53822fb0db0
cmderQ <1257698043@qq.com>
2017-11-26 22:30:58 +0800

    核心文件

---
+++ core/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Filename:  index.php
+ * User:      cmder
+ * Date:      2017/11/26
+ * Time:      22:24
+ */
+namespace  core;
+
+class index
+{
+    static  public function run()
+    {
+        echo 'OK';
+    }
+}
