13adcf9abafcb64f70ae889fabeeb05583ec3c48
cmderQ <1257698043@qq.com>
2017-11-27 23:23:14 +0800

    控制器

---
+++ app/controller/indexController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Filename:  index.class.php
+ * User:      cmder
+ * Date:      2017/11/27
+ * Time:      22:47
+ */
+namespace  app\controller;
+
+class indexController
+{
+    public function index()
+    {
+        op('It is indexController');
+    }
+}
