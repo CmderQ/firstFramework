ef7d56a6e14076da33fe5b1d2e112cf7b82f1339
cmderQ <1257698043@qq.com>
2017-11-29 00:08:18 +0800

    配置文件

---
+++ core/config/database.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 数据库配置文件
+ * Created by PhpStorm.
+ * Filename:  database.php
+ * User:      cmder
+ * Date:      2017/11/28
+ * Time:      23:50
+ */
+return [
+    'DSN' => 'mysql:host=localhost;dbname=test',
+    'USERNAME' => 'root',
+    'PASSWD' => '',
+];
---
+++ core/config/route.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 路由配置文件
+ * Created by PhpStorm.
+ * Filename:  route.php
+ * User:      cmder
+ * Date:      2017/11/28
+ * Time:      23:09
+ */
+return [
+    'Controller' => 'index',
+    'ACTION' => 'index'
+];
