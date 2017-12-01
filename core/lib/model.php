dc4f61baacd9683d622fe6adb613213c29611b12
cmderQ <1257698043@qq.com>
2017-12-02 00:14:14 +0800

    模型类

--- core/lib/model.php
+++ core/lib/model.php
@@ -9,15 +9,15 @@
 
 namespace core\lib;
 
+use core\lib\conf;
+
 class model extends \PDO
 {
     public function __construct()
     {
-        $dsn = 'mysql:host=localhost;dbname=test';
-        $username = 'root';
-        $passwd = '';
+        $database = conf::all('database');
         try {
-            parent::__construct($dsn, $username, $passwd);
+            parent::__construct($database['DSN'], $database['USERNAME'], $database['PASSWD']);
         } catch (\PDOException $e) {
             op($e->getMessage());
         }
