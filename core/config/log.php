2afe7c20059227003133c03c7e90af63d0b91c00
cmderQ <1257698043@qq.com>
2017-12-02 00:11:27 +0800

    日志

---
+++ core/config/log.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 日志驱动配置
+ * Created by PhpStorm.
+ * Filename:  log.php
+ * User:      cmder
+ * Date:      2017/11/30
+ * Time:      23:20
+ */
+
+return [
+    'DRIVE' => 'file',
+];
