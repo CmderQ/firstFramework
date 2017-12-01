b78a500bac87e1e4005d8be6d58379554ae6f059
cmderQ <1257698043@qq.com>
2017-12-02 00:14:47 +0800

    驱动

---
+++ core/lib/drive/log/file.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 文件系统
+ * Created by PhpStorm.
+ * Filename:  file.php
+ * User:      cmder
+ * Date:      2017/11/30
+ * Time:      23:18
+ */
+namespace core\lib\drive\log;
+
+class  file
+{
+    public function log($name)
+    {
+        op($name);
+    }
+}
---
+++ core/lib/drive/log/mysql.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * 数据库
+ * Created by PhpStorm.
+ * Filename:  mysql.php
+ * User:      cmder
+ * Date:      2017/11/30
+ * Time:      23:19
+ */
