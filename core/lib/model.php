d8862ae28e3662428384340168da8b7d63558bc2
cmderQ <1257698043@qq.com>
2017-12-04 23:35:51 +0800

    模型配置

--- core/lib/model.php
+++ core/lib/model.php
@@ -8,18 +8,13 @@
  */
 
 namespace core\lib;
-
 use core\lib\conf;
 
-class model extends \PDO
+class model extends  \Medoo\medoo
 {
     public function __construct()
     {
-        $database = conf::all('database');
-        try {
-            parent::__construct($database['DSN'], $database['USERNAME'], $database['PASSWD']);
-        } catch (\PDOException $e) {
-            op($e->getMessage());
-        }
+       $option = conf::all("database") ;
+       parent::__construct($option);
     }
 }
