e8c553b16b35541ce5afc2fc865edba945c2e729
cmderQ <1257698043@qq.com>
2017-12-07 00:05:36 +0800

    控制器

--- app/controller/indexController.php
+++ app/controller/indexController.php
@@ -10,21 +10,54 @@
 namespace app\controller;
 
 use app\model\ceshiModel;
+use app\model\guestbookModel;
 use core\index;
 
 class indexController extends index
 {
+    //所有留言
     public function index()
     {
-        $data = 'hello nihao';
+        $guestbook = new guestbookModel();
+        $data = $guestbook->all();
         $this->assign('data', $data);
         $this->display('index.html');
     }
 
-    public function hello()
+    //添加留言
+    public function add()
     {
-        $data = 'test views';
-        $this->assign('data', $data);
-        $this->display('hello.html');
+
+        $this->display('add.html');
+    }
+
+    //保存留言
+    public function save()
+    {
+        $data['title'] = post('title');
+        $data['content'] = post('content');
+
+        $guestbook = new guestbookModel();
+        $result = $guestbook->addOne($data);
+        if ($result) {
+            jump('/index/index');
+        }
+
+        op('留言失败');
+    }
+
+    //删除留言
+    public function del()
+    {
+        $id = get('id', 0, 'int');
+        if ($id) {
+            $model = new guestbookModel();
+            $retInfo = $model->delOne($id);
+            if ($retInfo) {
+                jump('/index/index');
+            }
+            op('删除失败');
+        }
+        exit('参数错误');
     }
 }
