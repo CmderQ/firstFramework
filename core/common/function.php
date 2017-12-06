777ce39fffde8608e6568ca5f3297524c0fe2895
cmderQ <1257698043@qq.com>
2017-12-07 00:04:50 +0800

    公共方法

--- core/common/function.php
+++ core/common/function.php
@@ -19,7 +19,75 @@ function op($var)
         var_dump(NULL);
     } else {
         echo "<pre style='position:relative;z-index:1000;padding:10px;border-radius:5px;
-        background:#F5F5F5;border:1px solid #aaa;font-size:14px;line-height:18px;opacity:0.9;'>"
+            background:#F5F5F5;border:1px solid #aaa;font-size:14px;line-height:18px;opacity:0.9;'>"
             . print_r($var, true) . "</pre>";
     }
+}
+
+/**
+ * @param $name 对应的值
+ * @param $default 默认值
+ * @param $filter 过滤方法
+ */
+function post($name, $default = false, $filter = false)
+{
+    if (isset($_POST[$name])) {
+        if ($filter) {
+            switch ($filter) {
+                case 'int':
+                    if (is_numeric($_POST[$name])) {
+                        return $_POST[$name];
+                    } else {
+                        return $default;
+                    }
+                    break;
+                default:
+                    ;
+            }
+        } else {
+            return $_POST[$name];
+        }
+    } else {
+        return $default;
+    }
+
+}
+
+/**
+ * @param $name 对应的值
+ * @param $default 默认值
+ * @param $filter 过滤方法
+ */
+function get($name, $default = false, $filter = false)
+{
+    if (isset($_GET[$name])) {
+        if ($filter) {
+            switch ($filter) {
+                case 'int':
+                    if (is_numeric($_GET[$name])) {
+                        return $_GET[$name];
+                    } else {
+                        return $default;
+                    }
+                    break;
+                default:
+                    ;
+            }
+        } else {
+            return $_GET[$name];
+        }
+    } else {
+        return $default;
+    }
+
+}
+
+/**
+ * 跳转方法
+ * @param $url
+ */
+function jump($url)
+{
+    header('location:' . $url);
+    exit();
 }
